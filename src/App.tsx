b223ffcb80ebcd6fab94f3b1ffd8e93fb0a05a93
Sean Purcell <me@seanp.xyz>
2018-09-21 22:45:27 -0400

    Add routing to example based on name

--- src/App.tsx
+++ src/App.tsx
@@ -5,49 +5,6 @@ import { Link } from 'react-router-dom';
 
 import { ExampleNames } from './Examples';
 
-/*
-// TODO(zhyty): these are just stubs. Will be need to be updated when Lessons
-// are implemented.
-interface IExercise {
-  title: string;
-  // we'll need to fill this out.
-  content: Snippet.ISnippetProps;
-}
-
-interface ILesson {
-  title: string;
-  exercises: IExercise[];
-  // we'll need to fill this out too.
-}
-
-function LessonFrame(lesson: ILesson) {
-  return (
-    <div className="lesson-frame">
-      <h1 className="lesson-frame-title">
-        Lesson: {lesson.title}
-      </h1>
-      <LessonInteractiveArea {...lesson} />
-    </div>
-  );
-}
-
-function LessonInteractiveArea(lesson: ILesson) {
-  const exercisesListItems = [];
-  for (const exercise of lesson.exercises) {
-    exercisesListItems.push(
-      <Snippet.Snippet key={exercise.title} {...exercise.content} />
-    );
-  }
-
-  return (
-    <div className="lesson-interactive">
-      Exercises:
-      <ul>{exercisesListItems}</ul>
-    </div>
-  );
-}
-*/
-
 function ExampleLinks() {
   const examples = [];
   for (const name of ExampleNames()) {
@@ -73,34 +30,5 @@ class App extends React.Component {
     );
   }
 }
-
-/*
-const SAMPLE_LESSON = {
-  exercises: [
-    {
-      title: 'Find the meaning of Schmubleck.',
-      content: {
-        blocks: [
-          {code: 'def main():\n', good: false},
-          {code: '    print("hello")\n    print("world")\n', good: true},
-        ],
-        lang: 'python',
-      },
-    },
-    {
-      title: 'Highlight the bad code in this sample.',
-      content: {
-        blocks: [
-          {code: 'int main() {\n', good: true},
-          {code: '    std::cout << "hi\\n";\n', good: false},
-          {code: '}', good: true},
-        ],
-        lang: 'clike',
-      },
-    },
-  ],
-  title: 'What is Yiddish Cornstarch?',
-};
-*/
 
 export default App;
