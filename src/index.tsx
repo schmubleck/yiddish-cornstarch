33a05200ebf0982d702d1ef8120f9119bfc729c6
Sean Purcell <me@seanp.xyz>
2018-09-21 21:47:41 -0400

    Fix build failure

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
-import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
 
-import App from './App';
-import {Example} from './Examples';
+import Routes from './Routes';
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact={true} path="/" component={App} />
-      <Route path="/example" component={Example} />
-    </div>
-  </Router>,
+  Routes(),
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
