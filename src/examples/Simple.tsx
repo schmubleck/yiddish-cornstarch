2423c9a42699092249a3061a3c318083bd230ed4
Sean Purcell <me@seanp.xyz>
2018-09-23 00:01:00 -0400

    Add parser for code samples

--- src/examples/Simple.tsx
+++ src/examples/Simple.tsx
@@ -2,8 +2,8 @@ const ex = {
     lang: "python",
     code:
 `
-\\Gdef happy_birthday(x):
-\\B    print("happy " + str(x) + "th birthday")
+@Gdef happy_birthday(x):
+@B    print("happy " + str(x) + "th birthday")
 `,
 };
 
