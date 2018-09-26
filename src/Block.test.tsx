3e0e3d92cead7b1ae78fa5e87bf3851c78d1c8b9
zhyty <tomyang711@gmail.com>
2018-09-26 17:41:23 -0400

    light refactoring and add block tests

---
+++ src/Block.test.tsx
@@ -0,0 +1,53 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { Block, BlockType } from './Block';
+
+describe('highlighting', () => {
+  const goodBlockProps = {
+    code: "print(\"You had me at hello\")",
+    typ: BlockType.Good,
+    language: "foo"
+  };
+
+  const badBlockProps = {
+    code: "print(\"You had me at goodbye\")",
+    typ: BlockType.Bad,
+    language: "bar"
+  };
+
+  const ignoreBlockProps = {
+    code: "print(\"You had me at schmubleck\")",
+    typ: BlockType.Ignore,
+    language: "schmubleck"
+  };
+
+  it('has no highlight before click', () => {
+    const goodBlockWrapper = shallow( <Block {...goodBlockProps} /> );
+    const badBlockWrapper = shallow( <Block {...badBlockProps} /> );
+    expect(goodBlockWrapper.is('.good')).toBe(false);
+    expect(goodBlockWrapper.is('.bad')).toBe(false);
+    expect(badBlockWrapper.is('.good')).toBe(false);
+    expect(badBlockWrapper.is('.bad')).toBe(false);
+  });
+
+  it('highlights to proper type after click', () => {
+    const goodBlockWrapper = shallow( <Block {...goodBlockProps} /> );
+    const badBlockWrapper = shallow( <Block {...badBlockProps} /> );
+    goodBlockWrapper.find('code').simulate('click');
+    expect(badBlockWrapper.is('.bad')).toBe(false);
+    expect(goodBlockWrapper.is('.good')).toBe(true);
+
+    badBlockWrapper.find('code').simulate('click');
+    expect(badBlockWrapper.is('.good')).toBe(false);
+    expect(badBlockWrapper.is('.bad')).toBe(true);
+  });
+
+  it('ignore blocks dont respond to clicks', () => {
+    const ignoreBlockWrapper = shallow( <Block {...ignoreBlockProps} /> );
+    expect(ignoreBlockWrapper.is('.good')).toBe(false);
+    expect(ignoreBlockWrapper.is('.bad')).toBe(false);
+    ignoreBlockWrapper.find('code').simulate('click');
+    expect(ignoreBlockWrapper.is('.good')).toBe(false);
+    expect(ignoreBlockWrapper.is('.bad')).toBe(false);
+  });
+});
