04e20122c462b837e86f22371e097b4d4de7a1c6
zhyty <tomyang711@gmail.com>
2018-09-27 19:56:21 -0400

    basic aesthetic improvements to start off

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import { HashRouter as Router, Link, Route } from 'react-router-dom';
 
+import 'bulma/css/bulma.css';
+
 import Registry from './ExampleRegistry';
 import { Example } from './Examples';
 
 const ExampleLinks = () => (
-  <div>
-    <h2>Examples</h2>
-    <ul>
+  <aside className="menu">
+    <p className="menu-label">Examples</p>
+    <ul className="menu-list">
       {Object.keys(Registry).map((name) =>
-        <li key={name}><Link to={"/examples/" + name}>{name}</Link></li>
+        <li key={name}><Link to={`/examples/${name}`}>{name}</Link></li>
       )}
     </ul>
-  </div>
+  </aside>
 );
 
 const App = () => (
   <Router>
     <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Yiddish Cornstarch</h1>
-      </header>
 
-      <ExampleLinks />
-      <Route path="/examples/:name" component={Example} />
+      <nav className="App-header navbar is-spaced has-shadow">
+        <div className="container">
+          <div className="navbar-brand">
+            <div className="navbar-item">
+              <h1 className="App-title title">
+                <Link to={"/"}>Yiddish Cornstarch</Link>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-fifth">
+              <ExampleLinks />
+            </div>
+            <div className="column">
+              <Route path="/examples/:name" component={Example} />
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   </Router>
 );
