b6ee0ef000c3241ffba454e7ee20fd462157aa86
Celine O'Neil <celineloneil@gmail.com>
2018-10-01 04:23:11 -0400

    Fix up example markup

--- src/examples/BasicLogic.tsx
+++ src/examples/BasicLogic.tsx
@@ -2,10 +2,10 @@ const ex = {
     lang: "python",
     code:
 `
-def calculate_shipping(state):
+@Gdef calculate_shipping(state):
 @B    if not (state != 'AK' and state != 'HI'):
-        return 30.0
-    return 20.0 
+@G        return 30.0
+@G    return 20.0
 `,
     feedback: "That logic up there is a bit hard to read. Fortunately, it's equivalent to check whether state is in the list ['AK', 'HI']. We think this is a lot easier to read.",
 };
